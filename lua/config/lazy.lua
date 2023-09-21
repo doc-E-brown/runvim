ddb77ddbb4f20bc557b64778ee2b524b1cc62ed8
Ben Johnston <docEbrown_github@protonmail.com>
2023-09-21 10:54:53 +1000

    feat: Vimwiki only
    Signed-off-by: Ben Johnston <docEbrown_github@protonmail.com>

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,7 +15,7 @@ vim.opt.rtp:prepend(lazypath)
 require("config.options")
 require("config.keymaps")
 require("config.autocommands")
-require("config.neovide")
+-- require("config.neovide")
 
 -- Use a protected call so we don't error out on first use
 local status_ok, lazy = pcall(require, "lazy")
